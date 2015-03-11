aa2af0053fb14143644f6f8e4b1ea06ce106380e
Tim Gunter <tim@vanillaforums.com>
2015-03-11 16:21:09 -0400

    Fix chart height

--- settings/about.php
+++ settings/about.php
@@ -17,7 +17,7 @@
 $ApplicationInfo['vfteamwork'] = [
     'Name' => 'Vanilla Hosting - Teamwork (ui)',
     'Description' => "Console plug-in application that provides a UI for Teamwork burndown.",
-    'Version' => '1.0',
+    'Version' => '1.0.1',
     'Author' => "Tim Gunter",
     'AuthorEmail' => 'tim@vanillaforums.com',
     'AuthorUrl' => 'http://about.me/timgunter',
